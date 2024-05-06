c0e00344dbf80d8330c868c6100aa72cedb52084
Mario Mastrandrea <m.mastrandrea@reply.it>
2024-05-06 20:31:52 +0200

    Initial Commit

---
+++ ApplePayIntegration/ApplePayIntegration.h
@@ -0,0 +1,18 @@
+//
+//  ApplePayIntegration.h
+//  ApplePayIntegration
+//
+//  Created by Mario Mastrandrea on 06/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApplePayIntegration.
+FOUNDATION_EXPORT double ApplePayIntegrationVersionNumber;
+
+//! Project version string for ApplePayIntegration.
+FOUNDATION_EXPORT const unsigned char ApplePayIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplePayIntegration/PublicHeader.h>
+
+
